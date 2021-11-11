0225a4a2247d627bafa29682e264808e7a750b5a
Bishoy Atef Sadek Zaky <bishoy.zaky@student.guc.edu.eg>
2021-11-11 19:13:13 +0200

    commenting on the main file code

--- Milestone 3/Test Codes/MainFile.m
+++ Milestone 3/Test Codes/MainFile.m
@@ -1,19 +1,36 @@
-Robot.number = 4;
-Robot.wheelRadius = 0.1;
-Robot.trackWidth = 1;
-Robot.originalRobotKinematics = differentialDriveKinematics("WheelRadius",Robot.wheelRadius,"TrackWidth",Robot.trackWidth,"VehicleInputs","VehicleSpeedHeadingRate");
+%This is the main file code to run the Simulated Annealing (SA)
+%optimization algorithm in solving the problem of Multi robot path planning
+%for dynamic formation application previously "Multi robot flocking control"
+%several tunable parameters can be changed to check their effect.
+%By: Team 9 (Ahmed Zaghloul, Abdelrahamn Saad, Bishoy Atef, John Gameel, Mona Farouk)
+%Date: Thursday, 11th November, 2021
+%=========================================================================
+
+%cleaning up the working environment before starting
+clc, clear, close all
+
+%Initialize Robot parameters:
+Robot.number = 4; %number of mobile robots    
+Robot.wheelRadius = 0.1; %radius of the robot wheel
+Robot.trackWidth = 1; %track width of the robot
+Robot.originalRobotKinematics = differentialDriveKinematics("WheelRadius"...
+    ,Robot.wheelRadius,"TrackWidth",Robot.trackWidth,"VehicleInputs",...
+    "VehicleSpeedHeadingRate"); %defining the kinematics of each robot 
 Robot.robots = repelem([Robot.originalRobotKinematics.copy()],Robot.number);
-Robot.initOrientation = (pi/4)*ones(Robot.number,1);
-Robot.initPosition = initializePositions(Robot.trackWidth*2,Robot.number);
-Robot.initPose = [Robot.initPosition Robot.initOrientation]';
-Robot.pathsColors = rand(Robot.number,3);
+%copying kinematics to each robot
+Robot.initOrientation = (pi/4)*ones(Robot.number,1); %Initialize the orientation of the robot
+Robot.initPosition = initializePositions(Robot.trackWidth*2,Robot.number); %Initialize the position of each robot
+Robot.initPose = [Robot.initPosition Robot.initOrientation]'; %Creating the pose of each robot
+Robot.pathsColors = rand(Robot.number,3); %Identify the color of each robot path
+
+%Setting controller parameters:
+Controller.desiredLinearVelocity = 3; %Set the desired linear velocity of the robots
+Controller.maxAngularVelocity = 2;  %Set the max angular velocity of the robots
+Controller.lookaheadDistance = 0.5; %Set the lookahead distance of the robots
+Controller.controllers = cell(1,Robot.number); %creating a cell array with the controllers
 
 
-Controller.desiredLinearVelocity = 3;
-Controller.maxAngularVelocity = 2;
-Controller.lookaheadDistance = 0.5;
-Controller.controllers = cell(1,Robot.number);
-for i = 1:Robot.number
+for i = 1:Robot.number %looping on each robot
     Controller.controllers{1,i} = controllerPurePursuit("DesiredLinearVelocity",Controller.desiredLinearVelocity,"MaxAngularVelocity",Controller.maxAngularVelocity,"LookaheadDistance",Controller.lookaheadDistance);
 end
 Controller.sampleTime = 0.1;
