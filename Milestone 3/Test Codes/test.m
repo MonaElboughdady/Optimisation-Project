ab5192f19481561bcc008d4709eb0c2fd845d867
Abdo0saad <65568425+Abdo0saad@users.noreply.github.com>
2021-12-01 01:05:15 +0200

    Update test.m

--- Milestone 3/Test Codes/test.m
+++ Milestone 3/Test Codes/test.m
@@ -1,12 +1,10 @@
-   if(GA.TS == 1)
-        GA.random_array = zeros(1,GA.K);  
-        for j = 1:R %for number of required parents
-            GA.random_indexes = randi([1,GA.Pop_size],[1,GA.K]); % get k random chromosoms to compare in a tournament
-            for i = 1:GA.K % for the K as is the number of randomaly selected chromosoms
-           GA.random_array(i) = GA.Fitness(GA.random_indexes(i)); 
-            end
-        [M,I] = min(GA.random_array); % get the index of the minimum fitness value and store in I
-         I = GA.random_indexes(I);
-        GA.parents_index(j) = I; %store the index of the best parent 
-        end 
+  GA.Offspring = cell(1,Robot.number);
+for i = 1:2:R %make R/2 crossovers
+    for j = 1:Robot.number
+      GA.Offspring{j}(:,:,i) = GA.Alpha * GA.Population_int{j}(:,:,GA.parents_index(i)) + (1-GA.Alpha) * GA.Population_int{j}(:,:,GA.parents_index(i+1)); %first child of the crossover
+      GA.Offspring{j}(:,:,i+1) = GA.Alpha * GA.Population_int{j}(:,:,GA.parents_index(i+1)) + (1-GA.Alpha) * GA.Population_int{j}(:,:,GA.parents_index(i)); %second child of the crossover
     end
+end
+for i = 1:Robot.number
+   GA.Population{i}(:,:,(GA.Pop_size* GA.Elite_ratio)+1:(GA.Pop_size* GA.Elite_ratio)+C) = GA.Offspring{i}(:,:,1:C); %add the children to the new generation
+end
