0a21f80c53fa47511152d7489824b5d3eefa9ff4
Abdo0saad <65568425+Abdo0saad@users.noreply.github.com>
2021-11-30 23:48:32 +0200

    Update Parents_selection.m

--- Milestone 3/Test Codes/Parents_selection.m
+++ Milestone 3/Test Codes/Parents_selection.m
@@ -43,14 +43,15 @@ function GA = Parents_selection(GA,R,indexes)
     end
     
 %% Tournament selection
-    if(GA.TS == 1)
-        random_array = zeros(1,GA.k);
-        random_indexes = randi([1,GA.Pop_size],[1,GA.k]); % get k random chromosoms to compare in a tournament
+     if(GA.TS == 1)
+        GA.random_array = zeros(1,GA.K);  
         for j = 1:R %for number of required parents
-        for i = 1:GA.k % for the K as is the number of randomaly selected chromosoms
-           random_array(i) = GA.Fitness(random_indexes(i)); 
-        end
-        [M,I] = min(random_array); % get the index of the minimum fitness value and store in I
+            GA.random_indexes = randi([1,GA.Pop_size],[1,GA.K]); % get k random chromosoms to compare in a tournament
+            for i = 1:GA.K % for the K as is the number of randomaly selected chromosoms
+           GA.random_array(i) = GA.Fitness(GA.random_indexes(i)); 
+            end
+        [M,I] = min(GA.random_array); % get the index of the minimum fitness value and store in I
+         I = GA.random_indexes(I);
         GA.parents_index(j) = I; %store the index of the best parent 
         end 
     end
