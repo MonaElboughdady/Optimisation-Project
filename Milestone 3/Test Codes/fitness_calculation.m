70ded8616da882dd81973aad20c14763eb64f7fa
Abdo0saad <65568425+Abdo0saad@users.noreply.github.com>
2021-11-30 07:42:38 +0200

    Update fitness_calculation.m

--- Milestone 3/Test Codes/fitness_calculation.m
+++ Milestone 3/Test Codes/fitness_calculation.m
@@ -3,7 +3,7 @@ function fitness_calculation(GA,Robot,Map,Controller)
         for i = 1:Robot.number 
             Controller.controllers{1,i}.Waypoints = GA.Population{i}(:,:,j); % assign the controller waypoints
         end
-    [pos,vel] = simulateKinematicsnew(Robot,Controller,Map,false);
-    GA.Fitness(1,j) = calculateCostFunction(pos,vel,Robot,Map,Controller,Robot.rf,Map.c);
+    [pos,vel] = simulateKinematicsnew(Robot,Controller,Map,false); %get the position and acceleration of each robot along the path
+    GA.Fitness(1,j) = calculateCostFunction(pos,vel,Robot,Map,Controller,Robot.rf,Map.c); %get the cosyt value
     end
 end
