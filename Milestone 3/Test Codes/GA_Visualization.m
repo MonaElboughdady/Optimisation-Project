5c7207bb434228d52ade5db9a797b48b25455590
Abdo0saad <65568425+Abdo0saad@users.noreply.github.com>
2021-12-02 06:08:09 +0200

    Update GA_Visualization.m

--- Milestone 3/Test Codes/GA_Visualization.m
+++ Milestone 3/Test Codes/GA_Visualization.m
@@ -2,7 +2,7 @@ function GA_Visualization(GA,Robot,Controller,Map)
 for j = 1:GA.Pop_size % for the size of the population 
         for i = 1:Robot.number 
             Controller.controllers{1,i}.Waypoints = GA.Population{i}(:,:,j); % assign the controller waypoints
-            visualize(Robot,Controller,Map)
         end
+         visualize(Robot,Controller,Map);
 end
 end
